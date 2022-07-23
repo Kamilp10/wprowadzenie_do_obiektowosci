386d2a8ef04d3dcd4cf7c4c086ce8b3011bc8067
Kamil <kamil.pietrzak4235@gmail.com>
2022-07-23 16:02:12 +0200

    obiekt Car

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,23 @@
 public class Car {
+    private String colour;
+    private int doorsAmount;
+    private String brand;
+    private boolean automaticGearBox;
+
+
+   public Car(String colour, int doorsAmount, String brand, boolean automaticGearBox){
+       this.colour=colour;
+       this.doorsAmount=doorsAmount;
+       this.brand=brand;
+       this.automaticGearBox=automaticGearBox;
+   }
+
+   public boolean setAutomaticGearBox(boolean automaticGearBox){
+       this.automaticGearBox=automaticGearBox;
+       return automaticGearBox;
+   }
+
+    public boolean getAutomaticGearBox() {
+        return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+
+        Car audi = new Car("RED",5,"Audi",false);
+        System.out.println(audi.getAutomaticGearBox());
+        audi.setAutomaticGearBox(true);
+        System.out.println(audi.getAutomaticGearBox());
+    }
 }
